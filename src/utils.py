de0e4a9fe28dbd973c376db874148f49470a63db
igorkonov <igorkonov1993@gmail.com>
2024-07-01 17:34:45 +0300

    update get_dump_json from utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,24 @@
 import json
 
 
-def get_dump_json(data: list[dict]) -> json:
-    with open('packages.json', 'w', encoding='utf-8') as f:
-        return json.dump(data, f, indent=2, ensure_ascii=False)
+
+def get_dump_json(data: Any, filename: str) -> None:
+    """
+    A function to dump JSON data to a file.
+    Parameters:
+        data: The JSON data to be dumped.
+        filename: The name of the file to dump the JSON data into.
+    """
+    if data:
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(
+                data,
+                f,
+                indent=2,
+                ensure_ascii=False,
+                default=lambda o: o.model_dump(),
+            )
+
+        log.success(f"Data saved to {filename}")
+    else:
+        log.error("No data available. No output file created.")
