a282afd1134372d998dbf07e5e57312ecf2d60f7
igorkonov <igorkonov1993@gmail.com>
2024-07-01 16:11:54 +0300

    add param branch_p10 + branch_sisyphus from config.py

--- src/config.py
+++ src/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     base_url: str = "https://rdb.altlinux.org/api"
+    branch_p10: str = "p10"
+    branch_sisyphus: str = "sisyphus"
 
 
 config = Config()
